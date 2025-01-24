5856611c7065fc17982302b4eb77f38ff8fa7982
tarahere <mtaraalleah@gmail.com>
2025-01-24 21:11:32 +0200

    code does not seem to be similar to the result on tutortial, look into more for tomorrow

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
 const menuBtnIcon = document.getElementById("i");
 
+//Not sure what I did wrong to not show the X mark when the menu is toggled
 menuBtn.addEventListener("click", (e) => {
     navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute(
+        "class",
+        isOpen ? "ri-close-line" : "ri-menu-3-line"
+    );
+});
+
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
+    menuBtn.setAttribute("class", "ri-menu-3-line");
 });
