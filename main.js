3e812a01ab1d9eefdc3634eb69be145c1ecf3e98
tarahere <mtaraalleah@gmail.com>
2025-01-28 10:28:30 +0200

    added a js library named scrollreveal to create an animated/dynamic look to the header elements

--- main.js
+++ main.js
@@ -17,3 +17,29 @@ navLinks.addEventListener("click", (e) => {
     navLinks.classList.remove("open");
     menuBtn.setAttribute("class", "ri-menu-3-line");
 });
+
+const scrollRevealOption = {
+    distance:"50px",
+    origin:"bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header_image img",{
+    ...scrollRevealOption,
+    origin: "right",
+});
+
+ScrollReveal().reveal(".header_content h1",{
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".header_content h2",{
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".header_btn",{
+    ...scrollRevealOption,
+    delay: 1500,
+});
